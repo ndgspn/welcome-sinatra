5ee16132e19e859d5632a23573851783ead7fa3d
ndgspn <nandangsopyan08@gmail.com>
2019-05-25 09:35:02 +0700

    first commit

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  "Hello Sinatra"
+  redirect to('/index')
+end
+
+get '/index' do
+  "This is an index"
+  @songs = [
+    {
+      title: 'Fly me to the moon',
+      url: "https://www.youtube.com/watch?v=mQR0bXO_yI8"
+    },
+    {
+      title: 'New York, New York',
+      url: "https://www.youtube.com/watch?v=btFfXgUdIzY"
+    },
+    {
+      title: 'My Way',
+      url: "https://www.youtube.com/watch?v=6E2hYDIFDIU"
+    }
+  ]
+  erb :default
+end
+
+get '/api/v1' do
+  content_type :json
+  {status: 'ok', messages: 'ok bro'}.to_json
+end
